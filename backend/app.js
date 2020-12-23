c6d3a1d28c770bc9f568660adeeb07db4abe2351
djanluka <cvitak.boris@gmail.com>
2020-12-23 15:51:23 +0100

    Drugi zadatak

--- backend/app.js
+++ backend/app.js
@@ -7,8 +7,39 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
-  console.log(req.body);
+products = new Map()
+
+app.post('/admin/unos-novog-proizvoda', (req, res) => {
+  users.push(req.body)
+  console.log('Lista proizvoda:')
+  users.forEach(u => console.log(u))
+  console.log()
+})
+
+
+products.set(0, {naziv: 'Mleko', opis: 'Slatko'})
+products.set(1, {naziv: 'Voda', opis: 'Slatko'})
+products.set(2, {naziv: 'Sok', opis: 'Slatko'})
+products.set(3, {naziv: 'Jogurt', opis: 'Slatko'})
+
+  
+function createJSONData() {
+    let data = {}
+    for (const [key, value] of products)
+        data[key] = value
+    return data;
+}
+
+app.get('/admin/proizvodi', (req, res) => {
+    let data = createJSONData()   
+    res.send(data)
+})
+
+app.delete('/admin/proizvodi', (req, res) => {
+    const id = req.body.id;
+    products.delete(id)
+    data = createJSONData();
+    res.send(data)
 })
 
 app.listen(port, () => {
--- matf-levi9-zadatak/src/App.js
+++ matf-levi9-zadatak/src/App.js
@@ -1,3 +1,4 @@
+/*
 import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -24,7 +25,7 @@ class App extends Component {
         console.log(this.state.naziv + " " + this.state.opis);
         
         if (this.state.naziv.length > 1 && this.state.opis.length > 1) {
-            await fetch('/', {
+            await fetch('/admin/unos-novog-proizvoda', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
@@ -94,3 +95,108 @@ class App extends Component {
 }
 
 export default App;
+*/
+
+
+
+
+import React, { Component } from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
+
+
+
+export default class App extends Component  {
+
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        rows: []
+      };
+      
+      this.renderMyData = this.renderMyData.bind(this);
+      this.handleDelete = this.handleDelete.bind(this);
+    }
+
+    componentDidMount() {
+        this.renderMyData();
+    }
+    
+
+    renderMyData(){
+        fetch('/admin/proizvodi')
+            .then((response) => response.json())
+            .then((responseJson) => {
+                let rows = []
+                for (const [key, value] of Object.entries(responseJson)) {
+                    const obj = {id: parseInt(key), naziv: value.naziv, opis: value.opis}
+                    rows.push(obj)
+                }
+                this.setState({rows: rows })
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+    }
+    
+    handleDelete = async (id) => {
+        const response = await fetch('/admin/proizvodi', {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({id: id}),
+            });
+        
+        response.json()
+            .then((responseJson) => {
+                let rows = []
+                for (const [key, value] of Object.entries(responseJson)) {
+                    const obj = {id: parseInt(key), naziv: value.naziv, opis: value.opis}
+                    rows.push(obj)
+                }
+                this.setState({rows: rows })
+            })
+    }
+    
+
+    render(){
+        if (this.state.rows !== 0) 
+               return (
+                <TableContainer component={Paper}>
+                  <Table className={'table'} aria-label="simple table">
+                    <TableHead>
+                      <TableRow>
+                        <TableCell >Id</TableCell>
+                        <TableCell >Naziv</TableCell>
+                        <TableCell >Opis</TableCell>
+                        <TableCell >Obrisi</TableCell>
+                      </TableRow>
+                    </TableHead>
+                    <TableBody>
+                      {this.state.rows.map((row) => (
+                        <TableRow key={row.id}>
+                          <TableCell >{row.id}</TableCell>
+                          <TableCell >{row.naziv}</TableCell>
+                          <TableCell >{row.opis}</TableCell>
+                          <TableCell >
+                            <Button variant="contained" color="secondary" onClick={()=>this.handleDelete(row.id)}> Delete</Button>
+                          </TableCell>
+                        </TableRow>
+                      ))}
+                    </TableBody>
+                  </Table>
+                </TableContainer>
+              )
+        else return (
+                 <h1> Loading </h1>
+            )
+    }
+}
