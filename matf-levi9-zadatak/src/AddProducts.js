1aca6ada8192abdf61704f625c1e1b292e710a26
djanluka <cvitak.boris@gmail.com>
2020-12-24 10:00:59 +0100

    Razdvojene url rute

---
+++ matf-levi9-zadatak/src/AddProducts.js
@@ -0,0 +1,97 @@
+
+import React, {Component} from 'react';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+import './Style.css'
+
+export default class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            naziv: '',
+            opis: '',
+        };
+        
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+    
+    
+    handleSubmit = async e => {
+        e.preventDefault();
+        console.log(this.state.naziv + " " + this.state.opis);
+        
+        if (this.state.naziv.length > 1 && this.state.opis.length > 1) {
+            await fetch('/admin/unos-novog-proizvoda', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ naziv: this.state.naziv, opis: this.state.opis }),
+            });
+        } else {
+            console.log('Niste uneli naziv ili opis')
+        }
+    };
+    
+    render() {
+        return (
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <div className={'paper'}>
+                <Typography component="h1" variant="h5">
+                  PROIZVODI
+                </Typography>
+                <form className={'form'} noValidate onSubmit={this.handleSubmit}>
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="naziv_proizvoda"
+                    label="Naziv proizvoda"
+                    name="naziv_proizvoda"
+                    autoComplete="naziv_proizvoda"
+                    autoFocus
+                    defaultValue={this.state.naziv}
+                    onChange={event => {
+                      const { value } = event.target;
+                      this.setState({ naziv: value });
+                    }}
+                  />
+                  <TextField
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="opis_proizvoda"
+                    label="Opis proizvoda"
+                    type="opis_proizvoda"
+                    id="opis_proizvoda"
+                    autoComplete="current-opis_proizvoda"
+                    defaultValue={this.state.opis}
+                    onChange={event => {
+                      const { value } = event.target;
+                      this.setState({ opis: value });
+                    }}
+                  />
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={'submit'}
+                  >
+                    Dodaj
+                  </Button>
+                  
+                </form>
+              </div>
+            </Container>
+        );
+    }
+}
+
--- matf-levi9-zadatak/src/index.js
+++ matf-levi9-zadatak/src/index.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App.js';
-import reportWebVitals from './reportWebVitals';
+import {Route, BrowserRouter} from 'react-router-dom'
+
+import App from './App'
+import AddProducts from './AddProducts'
+import TableProducts from './TableProducts'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Route exact path='/' component={App} />
+    <Route exact path='/admin/proizvodi' component={TableProducts} />
+    <Route exact path='/admin/unos-novog-proizvoda' component={AddProducts} />
+  </BrowserRouter>,
   document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+)
